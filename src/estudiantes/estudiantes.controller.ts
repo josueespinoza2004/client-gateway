85c62f1526cd2f5e379125a37cb2965a0de4a928
Josué Espinoza <142639857+josueespinoza2004@users.noreply.github.com>
2026-04-14 20:45:15 -0600

    feat: add getAll method to EstudiantesController for retrieving all students

--- src/estudiantes/estudiantes.controller.ts
+++ src/estudiantes/estudiantes.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Inject, Post } from '@nestjs/common';
+import { Body, Controller, Get, Inject, Post } from '@nestjs/common';
 import { ClientProxy } from '@nestjs/microservices';
 import { ESTUDIANTE_SERVICE } from 'src/config/service';
 import { CreateEstudianteDto } from './dto/create-estudiante.dto';
@@ -8,6 +8,11 @@ export class EstudiantesController {
   constructor(
     @Inject(ESTUDIANTE_SERVICE) private readonly estudianteClient: ClientProxy,
   ) {}
+
+  @Get()
+  getAll() {
+    return this.estudianteClient.send({ cmd: 'get_all_students' }, {});
+  }
 
   @Post()
   create(@Body() estudianteDto: CreateEstudianteDto) {
