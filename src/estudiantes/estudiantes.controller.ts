546973ca3e54d1ce47fe9c922852dbfe08f35c37
Josué Espinoza <142639857+josueespinoza2004@users.noreply.github.com>
2026-04-19 12:27:08 -0600

    feat: add update method to EstudiantesController for updating a student

--- src/estudiantes/estudiantes.controller.ts
+++ src/estudiantes/estudiantes.controller.ts
@@ -7,10 +7,14 @@ import {
   Param,
   ParseIntPipe,
   Post,
+  Put,
 } from '@nestjs/common';
 import { ClientProxy } from '@nestjs/microservices';
 import { ESTUDIANTE_SERVICE } from 'src/config/service';
-import { CreateEstudianteDto } from './dto/create-estudiante.dto';
+import {
+  CreateEstudianteDto,
+  UpdateEstudianteDto,
+} from './dto/create-estudiante.dto';
 
 @Controller('estudiantes')
 export class EstudiantesController {
@@ -31,6 +35,17 @@ export class EstudiantesController {
   @Get(':id')
   getOne(@Param('id', ParseIntPipe) id: number) {
     return this.estudianteClient.send({ cmd: 'get_one_student' }, id);
+  }
+
+  @Put(':id')
+  update(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() estudianteDto: UpdateEstudianteDto,
+  ) {
+    return this.estudianteClient.send(
+      { cmd: 'update_student' },
+      { id, estudianteDto },
+    );
   }
 
   @Delete(':id')
